f029f1121f8a3bb6bfc3137c1872630aa9ac7d64
Abdelrhman Khalifa <112504016+AbduKhalifa@users.noreply.github.com>
2023-12-23 05:17:28 +0200

    create routes
     community route & shop & chat & games

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createHashRouter } from 'react-router-dom';
 import './App.css';
 import Layout from './Components/Layout/Layout';
+import Community from './Components/Community/Community';
 
 function App() {
 
@@ -8,7 +9,25 @@ function App() {
   const router = createHashRouter([
     {
       path: "/",
-      element: <Layout />
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Community />
+        },
+        {
+          path:"/chats",
+          element: <h1> CHAT </h1>
+        },
+        {
+          path:"/games",
+          element: <h1> GAMES </h1>
+        },
+        {
+          path:"/shop",
+          element: <h1> SHOP </h1>
+        },
+      ]
     },
     {
       path: "*",
