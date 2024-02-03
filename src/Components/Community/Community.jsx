329955d47ea4a48e9847eff4b02529fc4fe025c8
Abdelrhman Khalifa <112504016+AbduKhalifa@users.noreply.github.com>
2024-02-03 11:21:14 +0200

    Update Community.jsx

--- src/Components/Community/Community.jsx
+++ src/Components/Community/Community.jsx
@@ -5,11 +5,13 @@ import React from 'react'
 import styles from "./community.module.css";
 import Intro from './ChildrenComponents/Intro/Intro';
 import Blogs from './ChildrenComponents/Blogs/Blogs';
+import { useSelector } from 'react-redux';
 
 
 export default function Community() {
 
-
+  const userData = useSelector(reducers => reducers.uReducer);
+  console.log(userData.nickname);
 
   return (
     <section>
