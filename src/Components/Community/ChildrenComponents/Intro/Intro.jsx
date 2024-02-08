1d117755e280fc7f5003e84d9fee674ff75020a6
Abdelrhman Khalifa <112504016+AbduKhalifa@users.noreply.github.com>
2024-02-08 10:22:12 +0200

    intro
    - Edit on intro section

--- src/Components/Community/ChildrenComponents/Intro/Intro.jsx
+++ src/Components/Community/ChildrenComponents/Intro/Intro.jsx
@@ -3,12 +3,6 @@
 
 
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
-
-
-// Import Swiper styles
-import 'swiper/css';
 import styles from "./intro.module.css";
 
 
@@ -16,50 +10,19 @@ export default function Intro() {
     return (
         <div className=' relative xl:mb-40 mb-20 2xl:mb-60'>
             <div className={styles.overlay + ' absolute top-0 left-0 w-full h-full z-10 '}>
-                <div className='flex items-center justify-center h-full flex-col'>
-                    <h1 className='font-semibold text-center sm:font-black text-[46px] sm:text-[80px] text-text'> GMZ COMMUNITY </h1>
-                    <p className='text-[38px] text-text sm:text-[70px]'> <span className='text-[38px] sm:text-[70px] text-helper'>4</span> EVER</p>
+                <div className={'flex items-center justify-center h-full flex-col f-1'}>
+                    <h1 className='font-semibold text-center sm:font-black text-[46px] sm:text-[80px] text-text f-1'> GMZ COMMUNITY </h1>
+                    <p className='text-[38px] text-text sm:text-[70px] f-1'> <span className='text-[40px] sm:text-[70px] text-base f-1'>4</span> EVER</p>
                 </div>
             </div>
-            <Swiper
-                className="mySwiper"
-                loop={true}
-                autoplay={{
-                    delay: 6000,
-                    disableOnInteraction: false,
-                }}
-                modules={[Autoplay]}
-                allowTouchMove={false}
-                speed={1300}
-            >
-                <SwiperSlide>
-                    <section className='w-full h-screen'>
-                        <img
-                            src={require("./../../../../assets/intro1.jpg")}
-                            alt="A person is playing"
-                            className='h-full object-cover object-center w-full'
-                        />
-                    </section>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <section className='w-full h-screen'>
-                        <img
-                            src={require("./../../../../assets/intro2.jpg")}
-                            alt="A person is playing"
-                            className='h-full object-cover object-center w-full'
-                        />
-                    </section>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <section className='w-full h-screen'>
-                        <img
-                            src={require("./../../../../assets/intro3.jpg")}
-                            alt="A person is playing"
-                            className='h-full object-cover object-center w-full'
-                        />
-                    </section>
-                </SwiperSlide>
-            </Swiper>
+            <section className='w-full h-screen'>
+                <img
+                    src={require("./../../../../assets/lol.gif")}
+                    alt="A person is playing"
+                    className='h-full object-cover object-center w-full'
+                />
+            </section>
+
         </div>
     )
 }
